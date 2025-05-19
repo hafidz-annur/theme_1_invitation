f7619d65cda1c70a265dd0760e0bf6c76d166696
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-05-19 21:16:27 +0700

    update tema 1 green

--- src/components/Galeri.vue
+++ src/components/Galeri.vue
@@ -7,13 +7,13 @@ const props = defineProps({ data: Object });
     <div class="relative h-[87dvh] bg-[#608961]/60 rounded-lg overflow-auto p-2">
       <v-row no-gutters>
         <template v-for="(image, imgIdx) in props.data?.galeri" :key="imgIdx">
-          <v-col class="pa-2" :cols="image.cols">
+          <v-col class="pa-1" :cols="image.cols">
             <v-img :src="image.foto" height="100%" rounded cover></v-img>
           </v-col>
 
-          <v-col v-if="image.children" class="d-flex flex-column pa-2" cols="6">
+          <v-col v-if="image.children" class="d-flex flex-column pa-1" cols="6">
             <v-row>
-              <v-col class="pa-2"
+              <v-col
                 v-for="(children, childIdx) in image.children"
                 :key="childIdx"
                 :cols="children.cols"
