6c04fa7d5a6d71238e48e55cfb2786cd90702b4b
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2025-06-07 10:38:36 +0700

    update master

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/theme_1_invitation/' : '/'
+  base: process.env.NODE_ENV === 'production' ? './' : '/'
 })
